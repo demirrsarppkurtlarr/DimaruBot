8ab5a466b0ecbd04be6973f37383cc317c6c2e5f
demirrsarppkurtlarr <demirrsarppkurtlarr@gmail.com>
2026-06-25 19:34:57 +0300

    fix prisma package exports: export generated client types directly

--- packages/prisma/src/index.ts
+++ packages/prisma/src/index.ts
@@ -4,4 +4,4 @@ export const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
 });
 
-export * from '@prisma/client';
+export * from '../prisma/client';
